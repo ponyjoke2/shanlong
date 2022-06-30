f6044b51b1e7f1dbd056670793e0a2b3db9c7c74
myHome <TimParker5357@gmail.com>
2022-06-30 10:58:47 +0800

    螺丝孔识别

---
+++ 直线图片/源.cpp
@@ -0,0 +1,198 @@
+﻿#include <opencv2/highgui/highgui_c.h>
+#include<opencv2/opencv.hpp>  
+#include<stdio.h>
+#include<iostream>  
+#include <string>    
+#include <vector>  
+#include <time.h>
+#include <ctime>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+double* LineMethod(Mat& img);
+
+int main(int argc, char** argv) {
+	//记录运行时间
+	clock_t start = 0;
+	clock_t end = 0;
+	start = clock();
+
+	//读入原图，返回Mat对象
+	Mat src = imread("直线//2.bmp");
+	Mat dst1, dstCopy1;
+
+	//判断图片是否读入成功
+	if (src.empty()) {
+		printf("could not load image...");
+		return -1;
+	}
+
+	//创建窗口并显示原图
+	namedWindow("src", CV_WINDOW_AUTOSIZE);
+	imshow("src", src);
+
+	//打印原图像属性
+	cout << "原图像属性" << endl;
+	//图像尺寸
+	cout << "size:" << src.size << endl;
+	//列宽
+	cout << "cols:" << src.cols << endl;
+	//行高
+	cout << "rows:" << src.rows << endl;
+	//维度
+	cout << "dims:" << src.dims << endl;
+	//通道数，RGB图为3通道，灰度图为单通道
+	cout << "channels:" << src.channels() << endl << endl;
+
+	//拷贝图片，dst1用于图像处理，dstCopy1用于显示识别出的圆
+	src.copyTo(dst1);
+	src.copyTo(dstCopy1);
+	
+	//调整图像分辨率，减少后期图像处理的计算量，n=0.25表示图片的宽和高均原图的四分之一
+	//width为调整分辨率后的图像宽度，heigth为调整分辨率后的图像高度
+	float n = 0.25;
+	int width = n * src.cols;
+	int height = n * src.rows;
+	Size size(width, height);
+	resize(dst1, dst1, size);
+	//namedWindow("resize", CV_WINDOW_AUTOSIZE);
+	//imshow("resize", dst1);
+
+	//ROI提取
+	// 设图片的左上角坐标为（0,0）,右下角坐标为(1,1)
+	// ROI的左上角坐标为(w1,h1),右下角坐标为(w2,h2)
+	float w1 = 0.4, w2 = 0.7, h1 = 0.4, h2 = 0.75;
+	Rect rect1(width * w1, height * h1, (w2 - w1) * width, (h2 - h1) * height);
+	dst1 = dst1(rect1);
+	namedWindow("ROI", CV_WINDOW_AUTOSIZE);
+	imshow("ROI", dst1);
+	
+	//直线检测
+	double* returnValue = LineMethod(dst1);
+	//判断是否有两条直线，有则计算交点坐标
+	if (returnValue[0] != -1 && returnValue[4] != -1) {
+		double k1 = (returnValue[3] - returnValue[1]) / (returnValue[2] - returnValue[0]);
+		double b1 = returnValue[1] - (returnValue[0] * k1);
+		double k2 = (returnValue[7] - returnValue[5]) / (returnValue[6] - returnValue[4]);
+		double b2 = returnValue[5] - (returnValue[4] * k2);
+
+		
+		long x=0, y=0;
+		//两条直线是不平行，则计算交点
+		if (k1 != k2) {
+			x = (b2 - b1) / (k1 - k2);
+			y = (k1 * b2 - b1 * k2) / (k1 - k2);
+		}
+		else {
+			cout << "两条直线平行，交点超出图像范围" << endl;
+			cv::waitKey(0);
+			return -2;
+		}
+		
+		//计算直线端点在原图像中的坐标
+		int v0 = (w1 * width + returnValue[0]) / n;
+		int v1 = (h1 * height + returnValue[1]) / n;
+		int v2 = (w1 * width + returnValue[2]) / n;
+		int v3 = (h1 * height + returnValue[3]) / n;
+		int v4 = (w1 * width + returnValue[4]) / n;
+		int v5 = (h1 * height + returnValue[5]) / n;
+		int v6 = (w1 * width + returnValue[6]) / n;
+		int v7 = (h1 * height + returnValue[7]) / n;
+
+		//在dstCopy1上画出直线
+		line(dstCopy1, Point(v0, v1), Point(v2, v3), Scalar(0, 0, 255), 2, LINE_AA);
+		line(dstCopy1, Point(v4, v5), Point(v6, v7), Scalar(0, 0, 255), 2, LINE_AA);
+
+		//交点像素横坐标和纵坐标
+		long Abscissa = (w1 * width + x) / n;
+		long Ordinate = (h1 * height + y) / n;
+		
+		//判断交点是否超出图像范围
+		if (Abscissa < dstCopy1.cols && y < dstCopy1.rows) {
+			cout << "交点横坐标：" << Abscissa << endl;
+			cout << "交点纵坐标：" << Ordinate << endl;
+			//在dstCopy1上画出交点
+			circle(dstCopy1, Point(Abscissa , Ordinate ), 4, Scalar(0, 0, 255), 2, LINE_AA);
+		}
+		else {
+			cout << "交点超出图像范围" << endl;
+			cv::waitKey(0);
+			return -3;
+		}
+		
+
+		
+		namedWindow("Point", CV_WINDOW_NORMAL);
+		imshow("Point", dstCopy1);
+	}
+	else {
+		cout << "直线不足两条" << endl;
+		cv::waitKey(0);
+		return -4;
+	}
+
+
+	end = clock();
+	cout << endl << "运行总时间: " << end - start << "ms";
+
+	cv::waitKey(0);
+	return 0;
+}
+
+/*直线检测方法
+* 参数：Mat& img（ROI图）
+* 返回值：ROI图中计算出的两条线段的四个端点坐标
+*/
+double* LineMethod(Mat& img) {
+	//创建一个数组，保存计算出的两条线段的四个端点坐标，作为函数的返回值
+	static double value[8] = { -1,-1,-1,-1,-1,-1,-1,-1};
+	Mat imgCopy;
+	img.copyTo(imgCopy);
+
+	//高斯模糊
+	GaussianBlur(img, img, Size(3, 3), 0, 0);
+	//namedWindow("GaussianBlur", CV_WINDOW_AUTOSIZE);
+	//imshow("GaussianBlur", img);
+
+	//转为灰度图
+	cvtColor(img, img, CV_BGR2GRAY);
+
+
+	//Canny边缘检测
+	Canny(img, img, 80, 200);
+	namedWindow("Canny", CV_WINDOW_AUTOSIZE);
+	imshow("Canny", img);
+
+	//霍夫直线变换
+    //直线检测结果保存在plines中
+	vector<Vec4f> plines;
+	HoughLinesP(img, plines, 1, CV_PI / 180, 50, 30, 100);
+
+	//遍历所有检测出的直线
+	for (int i = 0; i < plines.size(); i++) {
+		Vec4f houghLine = plines[i];
+		
+		//返回第一条和第二条条直线
+		if (i == 0) {
+			value[0] = houghLine[0];
+			value[1] = houghLine[1];
+			value[2] = houghLine[2];
+			value[3] = houghLine[3];
+		}
+		if (i == 1) {
+			value[4] = houghLine[0];
+			value[5] = houghLine[1];
+			value[6] = houghLine[2];
+			value[7] = houghLine[3];
+		}
+		//在ROI图中画出所有的线
+		line(imgCopy, Point(houghLine[0], houghLine[1]), Point(houghLine[2], houghLine[3]), Scalar(0, 0, 255), 1, LINE_AA);
+		printf("ROI图中直线%d起点和终点坐标：（%.1f，%.1f）（%.1f，%.1f)\n", i, houghLine[0], houghLine[1], houghLine[2], houghLine[3]);
+	}
+	namedWindow("HoughLinesP", CV_WINDOW_AUTOSIZE);
+	imshow("HoughLinesP", imgCopy);
+
+	return value;
+}
---
+++ 螺丝孔图片2/源.cpp
@@ -0,0 +1,225 @@
+﻿#include <opencv2/highgui/highgui_c.h>
+#include<opencv2/opencv.hpp>  
+#include<stdio.h>
+#include<iostream>  
+#include <string>    
+#include <vector>  
+#include <time.h>
+#include <ctime>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+int* circleMethod1(Mat& img);
+int* circleMethod2(Mat& img);
+
+
+int main(int argc, char** argv) {
+	//记录运行时间
+	clock_t start = 0;
+	clock_t end = 0;
+	start = clock();
+
+	//读入原图，返回Mat对象
+	Mat src = imread("2021-01-30//NG-2021-01-30 09;06;18.jpg");
+	Mat dst1, dstCopy1;
+
+	//判断图片是否读入成功
+	if (src.empty()) {
+		printf("could not load image...");
+		return -1;
+	}
+
+	//创建窗口并显示原图
+	namedWindow("src", CV_WINDOW_AUTOSIZE);
+	imshow("src", src);
+
+	//打印原图像属性
+	cout << "原图像属性" << endl;
+	//图像尺寸
+	cout << "size:" << src.size << endl;
+	//列宽
+	cout << "cols:" << src.cols << endl;
+	//行高
+	cout << "rows:" << src.rows << endl;
+	//维度
+	cout << "dims:" << src.dims << endl;
+	//通道数，RGB图为3通道，灰度图为单通道
+	cout << "channels:" << src.channels() << endl << endl;
+
+	//拷贝图片（深拷贝），用于图像处理
+	src.copyTo(dst1);
+
+	//调整图像分辨率，减少后期图像处理的计算量，n=0.25表示图片的宽和高均原图的四分之一
+	//width为调整分辨率后的图像宽度，heigth为调整分辨率后的图像高度
+	float n = 0.25;
+	int width = n * src.cols;
+	int height = n * src.rows;
+	Size size(width, height);
+	resize(dst1, dst1, size);
+	//拷贝图片（深拷贝），用于显示识别出的圆
+	dst1.copyTo(dstCopy1);
+	//namedWindow("resize", CV_WINDOW_AUTOSIZE);
+	//imshow("resize", dst1);
+
+	//ROI提取
+	// 设图片的左上角坐标为（0,0）,右下角坐标为(1,1)
+	//ROI的左上角坐标为(w1,h1),右下角坐标为(w2,h2)
+	float w1 = 0.25, w2 = 0.7, h1 = 0.2, h2 = 0.75;
+	//创建一个矩形，四个参数分别为左上角像素的横坐标，纵坐标，ROI宽度、高度。
+	Rect rect1(width * w1, height * h1, (w2 - w1) * width, (h2 - h1) * height);
+	dst1 = dst1(rect1);
+	namedWindow("ROI", CV_WINDOW_AUTOSIZE);
+	imshow("ROI", dst1);
+
+	
+	//计算圆心位置，返回一个数组。可选择方法一或方法二
+	// returnValue[0]为圆心在ROI图中的像素横坐标,returnValue[1]为圆心在ROI图中的像素纵坐标，returnValue[2]为圆在ROI图中的半径
+	int* returnValue =circleMethod1(dst1);
+	//int* returnValue = circleMethod2(dst1);
+	
+	//判断是否识别到圆
+	if (returnValue[0] > 0) {
+		//根据returnValue，计算原图中的像素坐标和半径
+		int Abscissa = (w1 * width + returnValue[0]) / n;
+		int Ordinate = (h1 * height + returnValue[1]) / n;
+		int radius = returnValue[2] / n;
+		printf("圆心横像素坐标：%d  圆心纵像素坐标：%d  圆半径：%d\n", Abscissa, Ordinate, radius);
+
+		//将识别到的圆和圆心显示在dstCopy1上
+		circle(dstCopy1, Point(Abscissa * n, Ordinate * n), radius * n, Scalar(0, 0, 255), 2, LINE_AA);
+		circle(dstCopy1, Point(Abscissa * n, Ordinate * n), 2, Scalar(0, 0, 255), 1, LINE_AA);
+		namedWindow("Circle", CV_WINDOW_AUTOSIZE);
+		imshow("Circle", dstCopy1);
+	}
+	else {
+		printf("未识别出圆\n");
+		cv::waitKey(0);
+		return -2;
+	}
+	
+
+
+
+	end = clock();
+	cout << endl << "运行总时间: " << end - start << "ms";
+
+	cv::waitKey(0);
+	return 0;
+}
+
+/*圆检测方法1
+* 参数：Mat& img（ROI图）
+* 返回值：ROI图中识别到的圆的圆心横坐标，圆心纵坐标，半径
+*/
+int* circleMethod1(Mat& img) {
+	//创建一个数组，保存圆心坐标和半径，作为函数的返回值
+	static int value[3] = { 0,0,0 };
+
+	//高斯模糊,去除噪声
+	GaussianBlur(img, img, Size(3, 3), 0, 0);
+	//namedWindow("GaussianBlur", CV_WINDOW_AUTOSIZE);
+	//imshow("GaussianBlur", img);
+
+	//将RGB图转为灰度图
+	cvtColor(img, img, CV_BGR2GRAY);
+
+
+	//二值化,将灰度图转为只有黑色和白色的二值图像
+	threshold(img, img, 28, 255, THRESH_BINARY_INV);
+	namedWindow("threshold", CV_WINDOW_AUTOSIZE);
+	imshow("threshold", img);
+
+	//形态学处理（开运算+闭运算）
+	//定义一个结构元素，用于开运算
+	Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3), Point(-1, -1));
+	//开运算，消除黑色背景上的白点
+	morphologyEx(img, img, CV_MOP_OPEN, kernel, Point(-1, -1), 2);
+	//namedWindow("open", CV_WINDOW_AUTOSIZE);
+	//imshow("open", img);
+
+	
+	//定义一个结构元素，用于闭运算
+	Mat kernel2 = getStructuringElement(MORPH_ELLIPSE, Size(4, 4), Point(-1, -1));
+	//闭运算，消除白色背景上的黑点
+	morphologyEx(img, img, CV_MOP_CLOSE, kernel2, Point(-1, -1), 2);
+	//namedWindow("close", CV_WINDOW_AUTOSIZE);
+	//imshow("close", img);
+
+
+	//圆检测，霍夫圆变换
+	//圆检测可以得到一系列圆的圆心坐标和半径，结果保存在pcircles中
+	vector<Vec3f> pcircles;
+	HoughCircles(img, pcircles, CV_HOUGH_GRADIENT, 1, 300, 100, 34, 60, 220);
+	
+	//圆检测完成后,图像转为RGB图,便于画圆
+	cvtColor(img, img, CV_GRAY2BGR);
+	//遍历找出的所有的圆
+	for (int i = 0; i < pcircles.size(); i++) {
+		Vec3f cc = pcircles[i];
+
+		//取第一个圆，即算法认为最接近圆的那个圆，返回圆心坐标和半径
+		if (i == 0) {
+			value[0] = cc[0];
+			value[1] = cc[1];
+			value[2] = cc[2];
+		}
+		printf("ROI图圆心:(%.2f,%.2f),半径：%.2f\n", cc[0], cc[1], cc[2]);
+
+		//在ROI图中画出所有的圆和圆心
+		circle(img, Point(cc[0], cc[1]), cc[2], Scalar(0, 0, 255), 2, LINE_AA);
+		circle(img, Point(cc[0], cc[1]), 4, Scalar(0, 0, 255), 2, LINE_AA);
+	}
+	namedWindow("HoughCircles1", CV_WINDOW_AUTOSIZE);
+	imshow("HoughCircles1", img);
+
+	
+	return value;
+}
+
+/*圆检测方法2
+* 参数：Mat& img（ROI图）
+* 返回值：ROI图中识别到的圆的圆心横坐标，圆心纵坐标，半径
+*/
+int* circleMethod2(Mat& img) {
+
+	static int value[3] = { 0,0,0 };
+
+	//高斯模糊
+	GaussianBlur(img, img, Size(3, 3), 0, 0);
+	//namedWindow("GaussianBlur", CV_WINDOW_AUTOSIZE);
+	//imshow("GaussianBlur", img);
+
+	//转为灰度图
+	cvtColor(img, img, CV_BGR2GRAY);
+
+	//圆检测，霍夫圆变换
+	//圆检测可以得到一系列圆的圆心坐标和半径，结果保存在pcircles中
+	vector<Vec3f> pcircles;
+	HoughCircles(img, pcircles, CV_HOUGH_GRADIENT, 1, 300, 100, 34, 60, 220);
+
+	//圆检测完成后,图像转为RGB图,便于画圆
+	cvtColor(img, img, CV_GRAY2BGR);
+	//遍历找出的所有的圆
+	for (int i = 0; i < pcircles.size(); i++) {
+		Vec3f cc = pcircles[i];
+
+		//取第一个圆，即算法认为最接近圆的那个圆，返回圆心坐标和半径
+		if (i == 0) {
+			value[0] = cc[0];
+			value[1] = cc[1];
+			value[2] = cc[2];
+		}
+		printf("ROI图圆心:(%.2f,%.2f),半径：%.2f\n", cc[0], cc[1], cc[2]);
+
+		//在ROI图中画出所有的圆和圆心
+		circle(img, Point(cc[0], cc[1]), cc[2], Scalar(0, 0, 255), 2, LINE_AA);
+		circle(img, Point(cc[0], cc[1]), 4, Scalar(0, 0, 255), 2, LINE_AA);
+	}
+	namedWindow("HoughCircles1", CV_WINDOW_AUTOSIZE);
+	imshow("HoughCircles1", img);
+
+
+	return value;
+}
